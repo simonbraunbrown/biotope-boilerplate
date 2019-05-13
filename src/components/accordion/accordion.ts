4d1af68c72ae30895b08d2a613ad15be582888f3
simonbraun <simon.braun@virtual-identity.com>
2019-05-13 13:42:20 +0200

    added accordion interaction logic in ts and some transition styling in css

--- src/components/accordion/accordion.ts
+++ src/components/accordion/accordion.ts
@@ -1,21 +1,53 @@
 interface AccordionOptions {}
 
 class Accordion {
-	private element: any;
+	private element: Element;
 	private options: AccordionOptions;
-    private defaults: AccordionOptions = {};
-    private textBox: HTMLElement[];
-	constructor(element: Element, options: AccordionOptions) {
-		this.element = element;
+	private defaults: AccordionOptions = {};
+	private accordionSections: HTMLElement[];
+	private textBox: HTMLElement[];
+	constructor(_element: Element, options: AccordionOptions) {
+		this.element = _element;
 		// ⚠️ remove options if you don't use them
-		this.options = { ...this.defaults, ...options };
+		this.options = {
+			...this.defaults,
+			...options
+		};
 		// init plugin
 		this.init();
+		this.setupEvents();
 	}
+
 	private init() {
-        this.textBox = this.element.querySelectorAll('.accordion__textBox');
-        this.textBox.forEach(e => e.innerHTML = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.');
-        
+		var self = this;
+
+		fetch('https://baconipsum.com/api/?type=meat-and-filler?format=text').then(function (response) {
+			return response.json();
+		}).then(function (json) {
+			var text: string = JSON.stringify(json);
+
+			self.textBox = Array.prototype.slice.call(self.element.querySelectorAll('.accordion__textBox'));
+			self.textBox.forEach(e => e.innerHTML = text);
+
+		});
+	}
+
+	private setupEvents() {
+		this.accordionSections = [].slice.call(this.element.querySelectorAll('.accordion__sectionContainer'));
+		this.accordionSections.forEach(e => e.addEventListener('click', (event) => {
+			console.log(e);
+			if (e.classList.contains('accordion__sectionContainer--toggled')) {
+				e.classList.remove('accordion__sectionContainer--toggled');
+				console.log("hey");
+			} else {
+				this.accordionSections.forEach((e) => {
+					e.classList.remove('accordion__sectionContainer--toggled');
+				});
+				if (e.className == 'accordion__sectionContainer') {
+					e.classList.add('accordion__sectionContainer--toggled');
+				}
+			}
+		}));
 	}
 }
 
