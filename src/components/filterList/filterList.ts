79e491623fcbf45fc1f5b17b7a06add8c5850256
simonbraun <simon.braun@virtual-identity.com>
2019-05-22 16:36:46 +0200

    refactoring, added obj to store event values

--- src/components/filterList/filterList.ts
+++ src/components/filterList/filterList.ts
@@ -17,6 +17,10 @@ class FilterList {
 	private element: Element;
 	private options: FilterListOptions;
 	private defaults: FilterListOptions = {};
+	private filterValues = {
+		textinput: '',
+		ageSlider: 1
+	}
 	private inputListSource: string = './components/filterList/people.json';
 	private inputList: Array<People> = [];
 	constructor(element: Element, options: FilterListOptions) {
@@ -28,32 +32,8 @@ class FilterList {
 	}
 	private init() {
 		// init plugin here
-		this.setupEvents();
 		this.fetchData();
-	}
-
-	private setupEvents() {
-		this.element.querySelector('.filterList__sortButton').addEventListener('click', event => {
-			this.filterList(this.inputList);
-		});
-
-		const ageSlider = this.element.querySelector('.filterList__ageRangeSlider');
-		ageSlider.addEventListener('change', event => {
-
-			//console.log(this.inputList);
-			const slider = <HTMLInputElement>event.target;
-			this.filterByAge(this.inputList, slider.value);
-		});
-
-		const searchInput = this.element.querySelector('.filterList__textInput');
-		searchInput.addEventListener('change', event => {
-			const text = <HTMLInputElement> event.target;
-			this.filterByName(this.inputList, text.value);
-		});
-		searchInput.addEventListener('keyup', event => {
-			const text = <HTMLInputElement> event.target;
-			this.filterByName(this.inputList, text.value);
-		});
+		this.setupEvents();
 	}
 
 	private fetchData() {
@@ -65,75 +45,104 @@ class FilterList {
 			});
 	}
 
-	private filterList(list: Array<People>) {
-		this.filterByAge(list, '0');
+	private setupEvents() {
+		this.element.querySelector('.filterList__sortButton').addEventListener('click', event => {
+			this.filterList(this.inputList);
+		});
+
+		const ageSlider = this.element.querySelector('.filterList__ageRangeSlider');
+		ageSlider.addEventListener('change', event => {
+			const slider = <HTMLInputElement>event.target;
+			this.filterValues.ageSlider = parseInt(slider.value);
+			this.filterList(this.inputList);
+			this.element.querySelector('.filterList__ageRangeSliderValue').innerHTML = 'filter by age from ' + this.filterValues.ageSlider + ' to 100';
+		});
+
+		const searchInput = this.element.querySelector('.filterList__textInput');
+		searchInput.addEventListener('change', event => {
+			const text = <HTMLInputElement>event.target;
+			this.filterValues.textinput = text.value;
+			this.filterList(this.inputList);
+		});
+
+		searchInput.addEventListener('keyup', event => {
+			const text = <HTMLInputElement>event.target;
+			this.filterValues.textinput = text.value;
+			this.filterList(this.inputList);
+		});
 	}
 
-	private filterByAge(unsortedList: Array<People>, age: string) {
-		if(age >= '1') {
-		const filterThis = (_list) => {
-			return _list.filter((item) => item.age >= age);
-		};
-		this.renderResults(filterThis(unsortedList));
-		this.element.querySelector('.filterList__ageRangeSliderValue').innerHTML = ' ' + age;
+
+	private filterList(list: Array<People>) {
+		let listToFilter = list;
+		console.log(this.filterValues.ageSlider);
+		listToFilter = this.filterByAge(listToFilter, this.filterValues.ageSlider);
+		listToFilter = this.filterByName(listToFilter, this.filterValues.textinput);
+		this.renderResults(listToFilter);
 	}
-	else {
-		this.renderResults(unsortedList);
-	}
+
+	private filterByAge(unsortedList: Array<People>, age: number) {
+		if (age >= 1) {
+			return unsortedList.filter((item) => item.age >= age);
+		}
+		else {
+			return unsortedList;
+		}
 	};
-		
+
 
 	private filterByName(unsortedList: Array<People>, name: string) {
-		const filterThis = (_list) => {
 			const regex = new RegExp(name, 'gi');
-			return _list.filter((item) => {
-				return item.name.first.match(regex) || item.name.last.match(regex);
-				console.log(regex);
-				
-				console.log(item.name.first);
+			
+			return unsortedList.filter((item) => {
+				return item.name.first.match(regex) || item.name.last.match(regex)
 			});
-		}
-		this.renderResults(filterThis(unsortedList));
 	}
 
 	private renderResults(filteredList: Array<People>) {
-		this.clearDom();
+		this.getEyeColor();
 
+		const filterItemTpl = (<any>window).biotope.configuration.get('tpl.filterItem');
+
+		let listItems = '';
 		filteredList.forEach(item => {
 
-			const newItemName = document.createElement("div");
-			newItemName.classList.add('filterList__itemName');
-			const newContentName = document.createTextNode(<string>item.name.first + ' ' + <string>item.name.last);
-			newItemName.appendChild(newContentName);
+			 const regex = new RegExp('('+this.filterValues.textinput+')', 'gi');
+			 let _matchName = item.name.first + ' ' + item.name.last;
+			 const matchName = _matchName.replace(regex,'<span class="hl">$1</span>');
 
+			let newItem = filterItemTpl({
+				name: item.name.first + ' ' + item.name.last,
+				matchName: matchName,
+				age: item.age,
+				eyeColor: item.eyeColor,
+				company: item.company
+			})
+			//console.log(newItem);
 
-			const newItemDetails = document.createElement("div");
-			newItemDetails.classList.add('filterList__itemDetails');
-			const newItemDetail = document.createElement("div");
-			newItemDetail.classList.add('filterList__itemDetail');
-			const newContentDetail = document.createTextNode(item.age + ' ' + <string>item.eyeColor + ' ' + <string>item.company);
-			newItemDetail.appendChild(newContentDetail);
-			newItemDetails.appendChild(newItemDetail);
+			listItems += newItem;
+			
+		});
+		const itemDisplayBox = document.querySelector('.filterList__display');
+		itemDisplayBox.innerHTML = listItems;
 
-
-			const newItem = document.createElement("div");
-			newItem.classList.add('filterList__item');
-			newItem.appendChild(newItemName);
-			newItem.appendChild(newItemDetails);
-
-
-			const itemDisplayBox = document.querySelector('.filterList__display');
-			itemDisplayBox.appendChild(newItem);
-		})
 	}
 
-	private clearDom() {
-		const items = this.element.querySelectorAll('.filterList__item');
-		if (items.length > 0) {
-			for (let i = 0; i < items.length; i++) {
-				items[i].remove();
+
+	private getEyeColor() {
+		const eyes = [];
+		for (let i = 0; i < this.inputList.length; i++) {
+			if (this.inputList[i].eyeColor === 'green') {
+				eyes.push(this.inputList[i].name.first + ' got ' + this.inputList[i].eyeColor + ' eyes')
 			}
 		}
+
+		let totalYears: number = this.inputList.reduce((sum, people) => {
+
+			return sum + people.age
+		}, 0);
+		
+
 	}
 }
 
